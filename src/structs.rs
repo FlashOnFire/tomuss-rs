53402b48ea5b085a971d55b00d9808d98302a9d8
FlashOnFire <flashonfire.dev@gmail.com>
2024-11-14 11:51:38 +0100

    feat: More parsing

--- src/structs.rs
+++ src/structs.rs
@@ -3,7 +3,7 @@ use rayon::iter::ParallelIterator;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-use crate::tomuss_deser_utils::deser_grades_vec;
+use crate::tomuss_deser_utils::{deser_grades_vec, deser_int_as_bool, WrappedStrF32};
 
 #[derive(Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -82,7 +82,7 @@ pub struct TomussData {
     p_template: Vec<serde_json::Value>,
 
     #[serde(deserialize_with = "deser_grades_vec")]
-    pub(crate) grades: Vec<serde_json::Value>,
+    pub(crate) grades: Vec<Grade>,
 
     profiling: HashMap<String, i32>,
 }
@@ -103,8 +103,80 @@ impl TomussData {
 
         serde_json::to_value(data)
             .ok()
-            .and_then(|x| TomussData::deserialize(x).ok())
+            .and_then(|x| serde_path_to_error::deserialize(x).unwrap())
             .unwrap()
     }
 }
 
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Grade {
+    masters: Vec<Person>,
+    rounding: i32,
+    contains_users: i32,
+    default_sort_column: Vec<i32>,
+    competence: String,
+    table_title: String,
+    popup_on_red_line: i32,
+    managers: Vec<String>,
+    code: i32,
+    bookmark: i32,
+    dates: Vec<f32>,
+    official_ue: i32,
+    ue: String,
+    year: i32,
+    columns: Vec<GradeColumn>,
+    line_id: String,
+    line: Vec<serde_json::Value>,
+    stats: serde_json::Value,
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct Person {
+    pub(crate) name: String,
+    pub(crate) surname: String,
+    pub(crate) mail: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
+enum Freezed {
+    #[default]
+    None,
+    F,
+    C,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct GradeColumn {
+    #[serde(rename = "type")]
+    type_: GradeType,
+    author: String,
+    #[serde(default)]
+    freezed: Freezed,
+    position: f32,
+    title: String,
+    width: Option<i32>,
+    comment: Option<String>,
+    the_id: String,
+    #[serde(deserialize_with = "deser_int_as_bool", default)]
+    hidden: bool,
+    repetition: Option<i32>,
+    weight: Option<WrappedStrF32>,
+    visibility_date: Option<String>,
+    green: Option<String>,
+    red: Option<String>,
+    enumeration: Option<String>,
+    cell_writable: Option<String>,
+    grade_type: Option<i32>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+enum GradeType {
+    Text,
+    Note,
+    Login,
+    Moy,
+    Prst,
+    Enumeration,
+    Upload,
+    Max,
+}
--- src/tomuss_deser_utils.rs
+++ src/tomuss_deser_utils.rs
@@ -1,14 +1,81 @@
-use serde::de;
-use serde::de::Error;
+use crate::structs::Grade;
+use crate::structs::Person;
+use serde::de::{Error, Unexpected};
+use serde::{de, Deserialize, Deserializer};
+use std::str::FromStr;
 
-pub(crate) fn deser_grades_vec<'de, D>(deserializer: D) -> Result<Vec<serde_json::Value>, D::Error>
+pub(crate) fn deser_grades_vec<'de, D>(deserializer: D) -> Result<Vec<Grade>, D::Error>
 where
     D: de::Deserializer<'de>,
 {
-    let vec: Vec<Vec<serde_json::Value>> = serde::Deserialize::deserialize(deserializer)?;
+    let vec: Vec<Vec<_>> = serde::Deserialize::deserialize(deserializer)?;
     if vec.is_empty() || vec.len() != 2 || !vec.get(1).unwrap().is_empty() {
         return Err(D::Error::custom("Grades deserializing error"));
     }
-    
-    Ok(vec.iter().flatten().cloned().collect::<Vec<_>>())
+
+    Ok(vec.iter().flatten().cloned().collect())
+}
+
+impl<'de> Deserialize<'de> for Person {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let vec: Vec<String> = serde::Deserialize::deserialize(deserializer)?;
+        if vec.len() != 3 {
+            return Err(D::Error::invalid_length(vec.len(), &"3"));
+        }
+        let mail = vec.get(2).unwrap();
+        if !mail.contains("@") {
+            return Err(D::Error::invalid_value(Unexpected::Str(mail), &"Missing @"));
+        }
+
+        Ok(Self {
+            name: vec.get(0).unwrap().clone(),
+            surname: vec.get(1).unwrap().clone(),
+            mail: mail.clone(),
+        })
+    }
+}
+
+pub(crate) fn deser_names_vec<'de, D>(deserializer: D) -> Result<Vec<Person>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let vec: Vec<Vec<_>> = serde::Deserialize::deserialize(deserializer)?;
+    if vec.is_empty() || vec.len() != 2 || !vec.get(1).unwrap().is_empty() {
+        return Err(D::Error::custom("Grades deserializing error"));
+    }
+
+    Ok(vec.iter().flatten().cloned().collect())
+}
+
+pub(crate) fn deser_int_as_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let n: i32 = serde::Deserialize::deserialize(deserializer)?;
+    match n {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => Err(D::Error::invalid_value(
+            Unexpected::Signed(n.into()),
+            &"zero or one",
+        )),
+    }
+}
+
+#[derive(Clone, Debug)]
+pub(crate) struct WrappedStrF32(f32);
+impl<'de> Deserialize<'de> for WrappedStrF32 {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let str = String::deserialize(deserializer)?;
+        match f32::from_str(str.as_str()) {
+            Ok(value) => Ok(Self(value)),
+            Err(_) => Err(D::Error::invalid_value(Unexpected::Str(&str), &"f32")),
+        }
+    }
 }
